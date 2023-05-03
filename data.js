671f1acfe07151492ef8dd9c2c3d50671c1ba859
ozwind <chewi0@hotmail.com>
2023-05-03 11:12:43 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,447 @@
+const quizCapital = [{
+        question: "Alabama",
+        options: ["Montgomery", "Birmingham", "Mobile", "Huntsville"]
+    },
+    {
+        question: "Alaska",
+        options: ["Juneau", "Anchorage", "Fairbanks", "Seward"]
+    },
+    {
+        question: "Arizona",
+        options: ["Phoenix", "Tucson", "Flagstaff", "Yuma"]
+    },
+    {
+        question: "Arkansas",
+        options: ["Little Rock", "Fort Smith", "Texarkana", "Clarksville"]
+    },
+    {
+        question: "California",
+        options: ["Sacramento", "San Francisco", "Los Angeles", "San Jose"]
+    },
+    {
+        question: "Colorado",
+        options: ["Denver", "Colorado Springs", "Fort Collins", "Boulder"]
+    },
+    {
+        question: "Connecticut",
+        options: ["Hartford", "New Haven", "Bridgeport", "Windsor"]
+    },
+    {
+        question: "Delaware",
+        options: ["Dover", "Wilmington", "Bridgeville", "Milton"]
+    },
+    {
+        question: "Florida",
+        options: ["Tallahassee", "Jacksonville", "Tampa", "Miami"]
+    },
+    {
+        question: "Georgia",
+        options: ["Atlanta", "Savannah", "Macon", "Athens"]
+    },
+    {
+        question: "Hawaii",
+        options: ["Honolulu", "Hilo", "Kailua-Kona", "Pearl Harbor"]
+    },
+    {
+        question: "Idaho",
+        options: ["Boise", "Twin Falls", "Coeur d'Alene", "Pocatello"]
+    },
+    {
+        question: "Illinois",
+        options: ["Springfield", "Chicago", "Peoria", "Rockford"]
+    },
+    {
+        question: "Indiana",
+        options: ["Indianapolis", "Fort Wayne", "Evansville", "Terre Haute"]
+    },
+    {
+        question: "Iowa",
+        options: ["Des Moines", "Cedar Rapids", "Sioux City", "Daven Port"]
+    },
+    {
+        question: "Kansas",
+        options: ["Topeka", "Kansas City", "Wichita", "Dodge City"]
+    },
+    {
+        question: "Kentucky",
+        options: ["Frankfort", "Lexington", "Louisville", "Bowling Green"]
+    },
+    {
+        question: "Louisiana",
+        options: ["Baton Rouge", "New Orleans", "Shreveport", "Lafayette"]
+    },
+    {
+        question: "Maine",
+        options: ["Augusta", "Bangor", "Portland", "Newport"]
+    },
+    {
+        question: "Maryland",
+        options: ["Annapolis", "Baltimore", "Rockville", "Gaithersburg"]
+    },
+    {
+        question: "Massachusetts",
+        options: ["Boston", "Worcester", "Springfield", "Salem"]
+    },
+    {
+        question: "Michigan",
+        options: ["Lansing", "Detroit", "Grand Rapids", "Ann Arbor"]
+    },
+    {
+        question: "Minnesota",
+        options: ["Saint Paul", "Minneapolis", "Rochester", "Duluth"]
+    },
+    {
+        question: "Mississippi",
+        options: ["Jackson", "Tupelo", "Biloxi", "Meridian"]
+    },
+    {
+        question: "Missouri",
+        options: ["Jefferson City", "St. Louis", "Columbia", "Kansas City"]
+    },
+    {
+        question: "Montana",
+        options: ["Helena", "Missoula", "Billings", "Bozeman"]
+    },
+    {
+        question: "Nebraska",
+        options: ["Lincoln", "Omaha", "Grand Island", "Scottsbluff"]
+    },
+    {
+        question: "Nevada",
+        options: ["Carson City", "Reno", "Las Vegas", "Henderson"]
+    },
+    {
+        question: "New Hampshire",
+        options: ["Concord", "Manchester", "Portsmouth", "Dover"]
+    },
+    {
+        question: "New Jersey",
+        options: ["Trenton", "Newark", "Princeton", "Morristown"]
+    },
+    {
+        question: "New Mexico",
+        options: ["Santa Fe", "Albuquerque", "Santa Rosa", "Gallup"]
+    },
+    {
+        question: "New York",
+        options: ["Albany", "New York City", "Syracuse", "Rochester"]
+    },
+    {
+        question: "North Carolina",
+        options: ["Raleigh", "Charlotte", "Fayetteville", "Durham"]
+    },
+    {
+        question: "North Dakota",
+        options: ["Bismarck", "Fargo", "Grand Forks", "Minot"]
+    },
+    {
+        question: "Ohio",
+        options: ["Columbus", "Cleveland", "Cincinnati", "Akron"]
+    },
+    {
+        question: "Oklahoma",
+        options: ["Oklahoma City", "Tulsa", "Moore", "Stillwater"]
+    },
+    {
+        question: "Oregon",
+        options: ["Salem", "Portland", "Eugene", "Medford"]
+    },
+    {
+        question: "Pennsylvania",
+        options: ["Harrisburg", "Philadelphia", "Pittsburgh", "Allentown"]
+    },
+    {
+        question: "Rhode Island",
+        options: ["Providence", "Portsmouth", "Bristol", "Warren"]
+    },
+    {
+        question: "South Carolina",
+        options: ["Columbia", "Charlestown", "Myrtle Beach", "Spartanburg"]
+    },
+    {
+        question: "South Dakota",
+        options: ["Pierre", "Sioux Falls", "Rapid City", "Watertown"]
+    },
+    {
+        question: "Tennessee",
+        options: ["Nashville", "Memphis", "Knoxville", "Chattanooga"]
+    },
+    {
+        question: "Texas",
+        options: ["Austin", "Dallas", "Houston", "San Antonio"]
+    },
+    {
+        question: "Utah",
+        options: ["Salt Lake City", "Provo", "Ogden", "St. George"]
+    },
+    {
+        question: "Vermont",
+        options: ["Montpelier", "Burligton", "Manchester", "St. Johnsbury"]
+    },
+    {
+        question: "Virginia",
+        options: ["Richmond", "Norfolk", "Newport News", "Lynchburg"]
+    },
+    {
+        question: "Washington",
+        options: ["Olympia", "Seattle", "Spokane", "Tacoma"]
+    },
+    {
+        question: "West Virginia",
+        options: ["Charleston", "Sutton", "Beckley", "Clarksburg"]
+    },
+    {
+        question: "Wisconsin",
+        options: ["Madison", "Milwaukee", "Green Bay", "Eau Claire"]
+    },
+    {
+        question: "Wyoming",
+        options: ["Cheyenne", "Casper", "Jackson", "Sheridan"]
+    }
+];
+
+const quizState = [{
+        question: "HuntsVille",
+        options: ["Alabama", "Mississippi", "Georgia", "Tennessee"]
+    },
+    {
+        question: "Seward",
+        options: ["Alaska", "New Jersey", "Montana", "Oregon"]
+    },
+    {
+        question: "Prescott",
+        options: ["Arizona", "Utah", "Idaho", "Nevada"]
+    },
+    {
+        question: "Texarkana",
+        options: ["Arkansas", "Texas", "Oklahoma", "Louisiana"]
+    },
+    {
+        question: "Eureka",
+        options: ["California", "Oregon", "Washington", "Idaho"]
+    },
+    {
+        question: "Pueblo",
+        options: ["Colorado", "New Mexico", "Arizona", "Texas"]
+    },
+    {
+        question: "Coeur d'Alene",
+        options: ["Idaho", "Washington", "Iowa", "Wisconsin"]
+    },
+    {
+        question: "Bozeman",
+        options: ["Montana", "Illinois", "Indiana", "Ohio"]
+    },
+    {
+        question: "Minot",
+        options: ["North Dakota", "Kansas", "Missouri", "Oklahoma"]
+    },
+    {
+        question: "St. Cloud",
+        options: ["Minnesota", "Michigan", "Vermont", "Kentucky"]
+    },
+    {
+        question: "Cedar Rapids",
+        options: ["Iowa", "Kansas", "Georgia", "Virginia"]
+    },
+    {
+        question: "Champaign",
+        options: ["Illinois", "Ohio", "Indiana", "Tennessee"]
+    },
+    {
+        question: "Lawrence",
+        options: ["Kansas", "Massachusetts", "Missouri", "New York"]
+    },
+    {
+        question: "Eugene",
+        options: ["Oregon", "Alabama", "Nevada", "Maine"]
+    },
+    {
+        question: "New Haven",
+        options: ["Connecticut", "Pennsylvania", "Michigan", "Oregon"]
+    },
+    {
+        question: "Ann Arbor",
+        options: ["Michigan", "Minnesota", "Wisconsin", "Ohio"]
+    },
+    {
+        question: "Bowling Green",
+        options: ["Kentucky", "Tennessee", "Wisconsin", "South Dakota"]
+    },
+    {
+        question: "Lynchburg",
+        options: ["Virginia", "South Carolina", "Alabama", "Louisiana"]
+    },
+    {
+        question: "Augusta",
+        options: ["Georgia", "Florida", "Alabama", "Tennessee"]
+    },
+    {
+        question: "Casper",
+        options: ["Wyoming", "Idaho", "Nevada", "Arizona"]
+    }
+];
+
+const quizCountryGivenFlag = [{  // https://flagpedia.net/index
+        flag: "br",
+        options: ["Brazil", "Panama", "Argentina", "Mexico"]
+    },
+    {
+        flag: "de",
+        options: ["Germany", "France", "Luxembourg", "Italy"]
+    },
+    {
+        flag: "fr",
+        options: ["France", "Germany", "Ireland", "Italy"]
+    },
+    {
+        flag: "au",
+        options: ["Australia", "Cook Islands", "United Kingdom", "New Zealand"]
+    },
+    {
+        flag: "at",
+        options: ["Austria", "Bolivia", "Bulgaria", "Hungary"]
+    },
+    {
+        flag: "es",
+        options: ["Spain", "Portugal", "Paraguay", "Brazil"]
+    },
+    {
+        flag: "ru",
+        options: ["Russia", "Austria", "Bulgaria", "Germany"]
+    },
+    {
+        flag: "fi",
+        options: ["Finland", "Iceland", "Norway", "Sweden"]
+    },
+    {
+        flag: "za",
+        options: ["South Africa", "Zimbabwe", "Sudan", "Mozambique"]
+    },
+    {
+        flag: "mx",
+        options: ["Mexico", "Italy", "Peru", "Romania"]
+    },
+    {
+        flag: "cr",
+        options: ["Costa Rica", "Nicaragua", "Paraguay", "Ecuador"]
+    },
+    {
+        flag: "cu",
+        options: ["Cuba", "Guyana", "Jordan", "Sudan"]
+    },
+    {
+        flag: "iq",
+        options: ["Iraq", "Iran", "Afghanistan", "Paktistan"]
+    },
+    {
+        flag: "kr",
+        options: ["South Korea", "Japan", "North Korea", "Philippines"]
+    },
+    {
+        flag: "cn",
+        options: ["China", "Russia", "Vietnam", "Laos"]
+    },
+    {
+        flag: "id",
+        options: ["Indonesia", "India", "Monaco", "Poland"]
+    },
+    {
+        flag: "in",
+        options: ["India", "Iran", "Iraq", "Kuwait"]
+    },
+    {
+        flag: "ar",
+        options: ["Argentina", "India", "Brazil", "Paraguay"]
+    },
+    {
+        flag: "ke",
+        options: ["Kenya", "Libya", "Ghana", "Tanzania"]
+    },
+    {
+        flag: "ch",
+        options: ["Switzerland", "Sweden", "Tonga", "Finland"]
+    }
+];
+
+const quizFlagOfCountry = [{
+        question: "France",
+        flags: ["fr", "de", "it", "ru"]
+    },
+    {
+        question: "Germany",
+        flags: ["de", "fr", "it", "hu"]
+    },
+    {
+        question: "Norway",
+        flags: ["no", "is", "fi", "se"]
+    },
+    {
+        question: "New Zealand",
+        flags: ["nz", "gb", "au", "ck"]
+    },
+    {
+        question: "Hungary",
+        flags: ["hu", "at", "bo", "bg"]
+    },
+    {
+        question: "Greece",
+        flags: ["gr", "ga", "hn", "lr"]
+    },
+    {
+        question: "Iceland",
+        flags: ["is", "no", "dk", "fi"]
+    },
+    {
+        question: "Malaysia",
+        flags: ["my", "lr", "gr", "th"]
+    },
+    {
+        question: "Taiwan",
+        flags: ["tw", "ve", "uy", "sg"]
+    },
+    {
+        question: "Poland",
+        flags: ["pl", "pe", "at", "id"]
+    },
+    {
+        question: "Ireland",
+        flags: ["ie", "it", "lu", "ng"]
+    },
+    {
+        question: "United Kingdom",
+        flags: ["gb", "nz", "ck", "au"]
+    },
+    {
+        question: "Greenland",
+        flags: ["gl", "hn", "fm", "so"]
+    },
+    {
+        question: "Cuba",
+        flags: ["cu", "jo", "mz", "ps"]
+    },
+    {
+        question: "Israel",
+        flags: ["il", "kp", "iq", "dz"]
+    },
+    {
+        question: "Japan",
+        flags: ["jp", "bd", "gl", "vn"]
+    },
+    {
+        question: "Netherlands",
+        flags: ["nl", "am", "ga", "hu"]
+    },
+    {
+        question: "Chile",
+        flags: ["cl", "cz", "mm", "pa"]
+    },
+    {
+        question: "Denmark",
+        flags: ["dk", "fo", "ua", "lu"]
+    },
+    {
+        question: "India",
+        flags: ["in", "ir", "ar", "tj"]
+    }
+];
---
+++ quiz.js
@@ -0,0 +1,207 @@
+let currentQuestion = 0;
+let score = 0;
+const quizes = [quizCapital, quizState, quizCountryGivenFlag, quizFlagOfCountry];
+var quizData = quizes[0];
+var incorrect = [];
+
+function init() {
+    $("#next").on("click", getNextQuestion);
+    $("#cancel").on("click", reset);
+    $("#continue").on("click", reset);
+    $("#start").on("click", startQuiz);
+    initData(quizCapital);
+    initData(quizState);
+    initData(quizCountryGivenFlag);
+    initData(quizFlagOfCountry);
+    reset();
+    changeSelectQuiz();
+
+    $("#selectQuiz").change(function(evt) {
+        quizData = quizes[Number(evt.target.value)];
+        changeSelectQuiz();
+    });
+}
+
+function initData(data) {
+    for (i in data) {
+        if (data[i].flags) {
+            data[i].options = [];
+            for (j in data[i].flags) {
+                data[i].options.push(toFlagUrl(data[i].flags[j]));
+            }                
+        }
+
+        data[i].answer = data[i].options[0];
+
+        if (data[i].flag) {
+            data[i].image = toFlagUrl(data[i].flag);
+        }
+    }
+}
+
+function changeSelectQuiz() {
+    var limit = $('#questionLimit');
+    limit.empty();
+
+    for (var i = 1; i <= quizData.length; i++) {
+        var option = $('<option></option>');
+        option.attr('value', i);
+        option.text(i);
+        limit.append(option);
+    }
+
+    limit.val(quizData.length);
+    incorrect = [];
+    document.title = $("#selectQuiz").find(":selected").text();
+}
+
+function getLimit() {
+    return Number($('#questionLimit').val());
+}
+
+function toFlagUrl(name) {
+    return "https://flagpedia.net/data/flags/h80/" + name + ".webp";
+}
+
+function showQuestion(questionIndex) {
+    var index = questionIndex ? questionIndex : currentQuestion;
+    const qData = quizData[index];
+    var question;
+    if (quizData == quizCapital) {
+        question = 'What is the capital of ' + qData.question + '?';
+    }
+    else if (quizData == quizState) {
+        question = qData.question + ' is in which state?';
+    }
+    else if (quizData == quizCountryGivenFlag) {
+        question = 'The flag is from which country?'
+    }
+    else if (quizData == quizFlagOfCountry) {
+        question = 'Which flag is from ' + qData.question + '?';
+    }
+    
+    var image = $('#image');
+    if (qData.image) {
+        image.attr('src', qData.image);
+    }
+    else {
+        image.removeAttr('src');
+    }
+    $("#select").hide();
+    $("#results").hide();
+    $("#question").text((index + 1) + ' of ' + getLimit() + ') ' + question);
+    $("#options div").remove();
+
+    shuffleArray(qData.options);
+
+    for (var i = 0; i < qData.options.length; i++) {
+        var div = $("<div></div>");
+        var radio = $("<input type='radio' name='option' value='" + i + "'>");
+        var item = quizData == quizFlagOfCountry
+                 ? $("<img>").attr("src", qData.options[i])
+                 : $("<label></label>").text(qData.options[i]);
+        div.append(radio, item);
+        $("#options").append(div);
+    }
+
+    $("#quiz").show();
+}
+
+function resetOptions() {
+    $('input[name="option"]').prop("checked", false);
+}
+
+function getNextQuestion() {
+    const selectedOption = $('input[name="option"]:checked').val();
+    const qData = quizData[currentQuestion];
+
+    if (selectedOption === undefined) {
+        alert("Please select an option.");
+        return;
+    }
+    var answer = qData.options[Number(selectedOption)];
+    if (answer === qData.answer) {
+        score++;
+    }
+    else {
+        //alert("Correct answer is " + qData.answer);
+        var item = {};
+        if (quizData == quizCapital) {
+            item.info = answer + " is not the capital of " + qData.question;
+        }
+        else if (quizData == quizState) {
+            item.info = qData.question + " is not in the state of " + answer;
+        }
+        else if (quizData == quizCountryGivenFlag) {
+            item.info = "is not the flag of " + answer;
+            item.flag = toFlagUrl(qData.flag);
+        }
+        else if (quizData == quizFlagOfCountry) {
+            item.info = "is not the flag of " + qData.question;
+            item.flag = answer;
+        }
+        if (Object.keys(item).length > 0) {
+            incorrect.push(item);
+        }
+    }
+    resetOptions();
+    currentQuestion++;
+
+    if (currentQuestion === getLimit()) {
+        showResults();
+    } else {
+        showQuestion(currentQuestion);
+    }
+}
+
+function reset() {
+    currentQuestion = 0;
+    score = 0;
+    incorrect = [];
+    $("#quiz").hide();
+    $("#results").hide();
+    $("#select").show();
+}
+
+function startQuiz() {
+    var index = $('#selectQuiz').val();
+    quizData = quizes[index];
+    shuffleArray(quizData);
+    showQuestion();
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function showResults() {
+    var limit = getLimit();
+    $("#quiz").hide();
+    var result = score === limit
+               ? `Perfect score. All ${limit} correct.`
+               : `${score} out of ${limit} questions answered correctly.`;
+
+    var incorrect$ = $('#incorrect');
+    incorrect$.empty();
+    for (var i in incorrect) {
+        var item = incorrect[i];
+        var flag;
+         if (item.flag) {
+            flag = $("<img>").attr("src", item.flag);
+        }
+        if (item.info) {
+            var elem = $("<div></div>");
+            var info = $("<p></p>").text(item.info);
+            if (flag) {
+                elem.append(flag);
+            }
+            elem.append(info);
+            incorrect$.append(elem);
+        }
+    }
+    $("#score").text(result).show();
+    $("#results").show();
+}
